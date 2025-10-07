dedf64f0095e8bc3e1d95fe553231a1b7d40e992
jonathan-fang <im.jonathan.fang@gmail.com>
2025-10-07 12:31:05 -0700

    4.2.3

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    # intakes filepath
+    with open(filepath) as f:
+        file_contents = f.read()
+    # output returns contents of file as string
+    return file_contents
+
+def main():
+    print(get_book_text('books/frankenstein.txt'))
+
+main()
