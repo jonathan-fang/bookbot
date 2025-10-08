3208470933fe7c6c63f3e8eebea5addb5318fca6
jonathan-fang <im.jonathan.fang@gmail.com>
2025-10-08 13:22:57 -0700

    refactored function char_count

--- stats.py
+++ stats.py
@@ -7,22 +7,16 @@ def book_text_to_word_count(filepath):
     # return number of words as integer
     return len(words)
 
+# addition to make sure characters and space counts, getting it checked by boots
 def book_text_to_char_count(filepath):
-    # initialize char_dict as empty dictionary
-    char_dict = {}
-    # input: filepath to book as string
-    with open(filepath) as f:
-        file_contents = f.read()
-    # split to get list of words
-    words = file_contents.split()
-    for word in words:
-        for char in word:
-            char = char.lower()
-            if char in char_dict:
-                char_dict[char] += 1
-            else:
-                char_dict[char] = 1
-    # convert any character to lowercase using .lower() method
-    # use a dictionary of string -> int
-    # returns number of times each character including symbols and spaces appear in the string
-    return char_dict
+    with open(filepath, "r") as f:
+        # you can stack object methods like this too
+        text = f.read().lower()
+    # initialize empty dictionary
+    counts = {}
+    for ch in text:
+        if ch in counts:
+            counts[ch] += 1
+        else:
+            counts[ch] = 1
+    return counts
