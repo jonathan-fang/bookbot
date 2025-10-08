d219c0fadc54826c1eec366edff5bcda87cb82fc
jonathan-fang <im.jonathan.fang@gmail.com>
2025-10-08 11:49:09 -0700

    4.2.4

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ def get_book_text(filepath):
     # output returns contents of file as string
     return file_contents
 
-def main():
-    print(get_book_text('books/frankenstein.txt'))
+def book_test_to_word_count(filepath):
+    with open(filepath) as f:
+        # turn filepath to string
+        file_contents = f.read()
+    # use .split method to count number of words
+    words = file_contents.split()
+    # return number of words as integer
+    return len(words)
 
-main()
+def main():
+    # print(get_book_text('books/frankenstein.txt'))
+    print(f"Found {book_test_to_word_count('books/frankenstein.txt')} total words")
+
+if __name__ == '__main__':
+    main()
