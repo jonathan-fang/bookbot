c2c00875cac9d4bc5873c33da2c7f1e24be0ac71
jonathan-fang <im.jonathan.fang@gmail.com>
2025-10-09 15:57:24 -0700

    mvp for pretty report 4.3.1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import book_text_to_word_count, book_text_to_char_count
+from stats import book_text_to_word_count, book_text_to_char_count, char_dict_to_sorted_list
 
 def get_book_text(filepath):
     # from 4.2.3 currently inactive
@@ -8,9 +8,19 @@ def get_book_text(filepath):
     # output returns contents of file as string
     return file_contents
 
+def pretty_report():
+    pass
+
 def main():
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {book_text_to_word_count('books/frankenstein.txt')} total words")
-    print(book_text_to_char_count('books/frankenstein.txt'))
+    print("--------- Character Count -------")
+    for item in char_dict_to_sorted_list(book_text_to_char_count('books/frankenstein.txt')):
+        if item['char'].isalpha(): # only include alphanumeric characters
+            print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 if __name__ == '__main__':
     main()
--- stats.py
+++ stats.py
@@ -13,10 +13,32 @@ def book_text_to_char_count(filepath):
         # you can stack object methods like this too
         text = f.read().lower()
     # initialize empty dictionary
-    counts = {}
+    char_dict = {}
     for ch in text:
-        if ch in counts:
-            counts[ch] += 1
+        if ch in char_dict:
+            char_dict[ch] += 1
         else:
-            counts[ch] = 1
-    return counts
+            char_dict[ch] = 1
+    return char_dict
+
+def char_dict_to_sorted_list(char_dict):
+    # input char_dict
+    # list of dictionaries
+    list_of_dicts = []
+    for char in char_dict:
+        # create tiny dictionary for each character value pair
+        small_char_dict = {} # init
+        # add key value pairs to small_char_dict
+        small_char_dict["char"] = char
+        # start with trying to make the whole function work then make helper functions?
+        small_char_dict["num"] = char_dict[char]
+        list_of_dicts.append(small_char_dict)
+        # if char.isalpha(): # only include alphanumeric characters
+    # output sorted list of dictionaries
+    list_of_dicts.sort(reverse=True, key=find_num_key)
+    return list_of_dicts
+
+def find_num_key(char_dict):
+    # helper function
+    # input char_dict and key
+    return char_dict["num"]
