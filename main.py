4060ed9db521f0e1b428ef3d0b6154c1ff5d3ac8
jonathan-fang <im.jonathan.fang@gmail.com>
2025-10-09 17:39:18 -0700

    sys.argv 4.3.2

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
+import sys
 from stats import book_text_to_word_count, book_text_to_char_count, char_dict_to_sorted_list
 
-# def get_book_text(filepath):
-#     # from 4.2.3 currently inactive
-#     # intakes filepath
-#     with open(filepath) as f:
-#         file_contents = f.read()
-#     # output returns contents of file as string
-#     return file_contents
-
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {filepath}...")
     print("----------- Word Count ----------")
-    print(f"Found {book_text_to_word_count('books/frankenstein.txt')} total words")
+    print(f"Found {book_text_to_word_count(filepath)} total words")
     print("--------- Character Count -------")
-    for item in char_dict_to_sorted_list(book_text_to_char_count('books/frankenstein.txt')):
+    for item in char_dict_to_sorted_list(book_text_to_char_count(filepath)):
         if item['char'].isalpha(): # only include alphanumeric characters
             print(f"{item['char']}: {item['num']}")
     print("============= END ===============")
